fdb582d72022e96867bc2b14a8a25d6f8d5d746e
Facundo Crego <facundo.crego@gmail.com>
2015-11-22 00:40:48 -0300

    first commit

---
+++ jsinheritance.js
@@ -0,0 +1,54 @@
+
+var classes = {
+  'Object' : Object
+};
+
+var Define = function(child) {
+
+  var name = child.name;
+  console.log('Class name: ' + name);
+  if (name === 'Function') {
+    throw 'Error: Cannot define an unnamed class.';
+  }
+
+  classes[name] = child;
+
+  var extend = function(child, parent) {
+    for (var i in parent.prototype) {
+      child.prototype[i] = parent.prototype[i];
+    }
+  };
+
+
+  return {
+    as : function(parents) {
+      if ( !( (typeof parents) === "object" && parents.length) ) {
+        parents = [parents];
+      }
+      child.parents = {};
+
+      parents.forEach(function(parent) {
+        console.log('Parent: ' + parent);
+        var Parent = classes[parent];
+        if (!Parent) {
+          throw 'Parent ' + parent + ' is not defined.'
+        }
+        extend(child, Parent);
+        child.parents[parent] = Parent.prototype;
+      });
+
+      child.prototype.constructor = child;
+
+      return function(/*this, arguments*/) {
+        var args = Array.prototype.slice.call(arguments);
+        var self = args.shift();
+        parents.forEach(function(parent) {
+            var Parent = classes[parent];
+            Parent.apply(self, args);
+        });
+      };
+    }
+  };
+};
+
+module.exports = Define;
